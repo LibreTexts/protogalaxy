2a2de63463ddb30068f5488159bcd7401b7162e0
rkevin <rk@rkevin.dev>
2020-07-15 12:04:57 -0700

    a bunch of random broken stuff that im just committing so noah can see it

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+# @summary Main class for protogalaxy related stuff
+#
+# @example TODO
+#
+# @param k8s_version
+#   Version of the kubeadm, kubelet and kubectl packages to install,
+#   along with the control plane version. Defaults to 1.18.5.
+#
+class protogalaxy (
+  $k8s_version = '1.18.5',
+) {
+
+}
