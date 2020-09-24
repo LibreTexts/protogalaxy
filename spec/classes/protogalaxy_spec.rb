c863ed50d1708c432be9457e19e198163bf3396a
rkevin <rk@rkevin.dev>
2020-09-23 20:17:17 -0700

    placate linter

--- spec/classes/protogalaxy_spec.rb
+++ spec/classes/protogalaxy_spec.rb
@@ -6,45 +6,63 @@ describe 'protogalaxy' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do {
-      } end
+      let(:params) do
+        {
+        }
+      end
+
       it { is_expected.to compile }
     end
     context "init control plane node on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do {
-        'role' => 'initial_control'
-      } end
+      let(:params) do
+        {
+          'role' => 'initial_control',
+        }
+      end
+
       it { is_expected.to compile }
     end
     context "additional control plane node on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do {
-        'role' => 'control'
-      } end
+      let(:params) do
+        {
+          'role' => 'control',
+        }
+      end
+
       it { is_expected.to compile }
     end
     context "init control plane node with custom interface on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do {
-        'role' => 'initial_control',
-        'network_interface' => os_facts[:networking]['primary']
-      } end
+      let(:params) do
+        {
+          'role' => 'initial_control',
+          'network_interface' => os_facts[:networking]['primary'],
+        }
+      end
+
       it { is_expected.to compile }
     end
     context "additional control plane node with custom interface on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do {
-        'role' => 'control',
-        'network_interface' => os_facts[:networking]['primary']
-      } end
+      let(:params) do
+        {
+          'role' => 'control',
+          'network_interface' => os_facts[:networking]['primary'],
+        }
+      end
+
       it { is_expected.to compile }
     end
     context "worker node on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do {
-        'role' => 'worker'
-      } end
+      let(:params) do
+        {
+          'role' => 'worker',
+        }
+      end
+
       it { is_expected.to compile }
     end
   end
