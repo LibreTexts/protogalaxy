d1dc786533fc1a94e6ce6760c73dfe64fb44f762
rkevin <rk@rkevin.dev>
2020-09-30 05:46:07 -0700

    fix move residue, placate linter

--- manifests/bootstrap/reset.pp
+++ manifests/bootstrap/reset.pp
@@ -1,11 +1,11 @@
-# @summary A short summary of the purpose of this class
+# @summary Runs kubeadm reset to tear down the cluster
 #
 # @example
-#   include protogalaxy::cluster_reset
+#   include protogalaxy::bootstrap::reset
 
-class protogalaxy::cluster_reset {
+class protogalaxy::bootstrap::reset {
   exec { 'kubeadm reset':
     command => '/usr/bin/kubeadm reset -f',
-    onlyif => 'test -e /etc/kubernetes/kubelet.conf',
+    onlyif  => 'test -e /etc/kubernetes/kubelet.conf',
   }
 }
