7b2b13b9e2046a143835ed90a9a4645463a6fdc2
sandertyu <noasanders@ucdavis.edu>
2021-07-22 17:53:28 -0700

    try to run service as user

--- manifests/management_jobs.pp
+++ manifests/management_jobs.pp
@@ -4,24 +4,30 @@
 #   include protogalaxy::management_jobs.pp
 
 class protogalaxy::management_jobs inherits protogalaxy {
-  file { '/etc/systemd/system/minesweep-systemd.service':
+  file { '/home/milky/.config/systemd/user':
+    ensure  => directory,
+    owner   => 'milky',
+    group   => 'milky',
+    mode    => '0755',
+  }
+  file { '/home/milky/.config/systemd/user/minesweep-systemd.service':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
+    owner   => 'milky',
+    group   => 'milky',
     mode    => '0644',
     content => file('protogalaxy/minesweep-systemd.service'),
   }
-  file { '/etc/systemd/system/minesweep-systemd.timer':
+  file { '/home/milky/.config/systemd/user/minesweep-systemd.timer':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
+    owner   => 'milky',
+    group   => 'milky',
     mode    => '0644',
     content => file('protogalaxy/minesweep-systemd.timer'),
   }
-  file { '/etc/systemd/system/minesweep-systemd.sh':
+  file { '/home/milky/.config/systemd/user/minesweep-systemd.sh':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
+    owner   => 'milky',
+    group   => 'milky',
     mode    => '0755',
     content => file('protogalaxy/minesweep-systemd.sh'),
   }
