e4a5deac6e6dadf41799181cfbca890dce7cebc6
sandertyu <noasanders@ucdavis.edu>
2021-07-26 15:10:50 -0700

    pgrep -lf

--- files/minesweep-systemd.sh
+++ files/minesweep-systemd.sh
@@ -2,9 +2,9 @@
 
 kubectl get pods -n binderhub | awk '/jupyter-/{print $1}' | while read POD; do
     cat /home/milky/miners.txt | while read LINE; do
-        kubectl exec -n binderhub -c notebook $POD -- pgrep -l "$LINE" 2> /dev/null
+        kubectl exec -n binderhub -c notebook $POD -- pgrep -lf "$LINE" 2> /dev/null
         if [ "$?" -eq 0 ]; then
-            PID=$(kubectl exec -n binderhub -c notebook $POD -- pgrep "$LINE")
+            PID=$(kubectl exec -n binderhub -c notebook $POD -- pgrep -lf "$LINE")
             date >> /home/milky/miner-log.txt
             kubectl exec -n binderhub -c notebook $POD -- \
             ps $PID | awk 'NR>1' | awk '{$1=$2=$3=$4=""; print $0}' | awk '{$1=$1;print}' >> /home/milky/miner-log.txt
