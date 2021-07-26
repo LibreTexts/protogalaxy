bcac06e00896cecdfa4fa698ada66def1401f811
sandertyu <noasanders@ucdavis.edu>
2021-07-26 16:02:39 -0700

    use tee cuz /var/log is write protected

--- files/minesweep-systemd.sh
+++ files/minesweep-systemd.sh
@@ -5,9 +5,9 @@ kubectl get pods -n binderhub | awk '/jupyter-/{print $1}' | while read POD; do
         kubectl exec -n binderhub -c notebook $POD -- pgrep -lf "$LINE" 2> /dev/null
         if [ "$?" -eq 0 ]; then
             PID=$(kubectl exec -n binderhub -c notebook $POD -- pgrep -f "$LINE")
-            date >> /home/milky/miner-log.txt
+            date | sudo tee /var/log/miner.log > /dev/null
             kubectl exec -n binderhub -c notebook $POD -- \
-            ps $PID | awk 'NR>1' | awk '{$1=$2=$3=$4=""; print $0}' | awk '{$1=$1;print}' >> /home/milky/miner-log.txt
+            ps $PID | awk 'NR>1' | awk '{$1=$2=$3=$4=""; print $0}' | awk '{$1=$1;print}' | sudo tee /var/log/miner.log > /dev/null
             kubectl delete pod -n binderhub $POD 2> /dev/null
             break
         fi
